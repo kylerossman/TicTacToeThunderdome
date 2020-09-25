a6761adbcd6243946263c8467817ef48e3c09b91
Kyle Rossman <kylerossman@Kyles-MacBook-Pro.local>
2020-09-25 01:09:09 -0400

    Added logic checking if user won

--- TicTacToe/src/main/java/TicTacToe/Game.java
+++ TicTacToe/src/main/java/TicTacToe/Game.java
@@ -30,4 +30,39 @@ public class Game {
 
         this.gameBoxes = newGameBoxes;
     }
+
+    public boolean didUserWin(int i) {
+        int box0 = this.getGameBoxes().get(0).getTeam();
+        int box1 = this.getGameBoxes().get(1).getTeam();
+        int box2 = this.getGameBoxes().get(2).getTeam();
+        int box3 = this.getGameBoxes().get(3).getTeam();
+        int box4 = this.getGameBoxes().get(4).getTeam();
+        int box5 = this.getGameBoxes().get(5).getTeam();
+        int box6 = this.getGameBoxes().get(6).getTeam();
+        int box7 = this.getGameBoxes().get(7).getTeam();
+        int box8 = this.getGameBoxes().get(8).getTeam();
+
+        boolean didWin = false;
+
+        if (box0 == i && box1 == i && box2 == i) {
+            return true;
+        } else if (box3 == i && box4 == i && box5 == i) {
+            return true;
+        } else if (box6 == i && box7 == i && box8 == i) {
+            return true;
+        } else if (box0 == i && box3 == i && box6 == i) {
+          return true;
+        } else if (box1 == i && box4 == i && box7 == i) {
+            return true;
+        } else if (box2 == i && box5 == i && box8 == i) {
+            return true;
+        } else if (box0 == i && box4 == i && box8 == i) {
+            return true;
+        } else if (box2 == i && box4 == i && box6 == i) {
+            return true;
+        }
+
+        return didWin;
+
+    }
 }
--- TicTacToe/src/test/java/TicTacToeObjectTest.java
+++ TicTacToe/src/test/java/TicTacToeObjectTest.java
@@ -45,4 +45,52 @@ public class TicTacToeObjectTest {
         assertEquals(0, game.getGameBoxes().get(0).getTeam());
     }
 
+    @Test
+    public void checkForWinTest() {
+        Game game = new Game(new ArrayList<Space>());
+
+        game.instantiateSpaces();
+
+        game.getGameBoxes().get(0).setTeam(1);
+        game.getGameBoxes().get(1).setTeam(1);
+        game.getGameBoxes().get(2).setTeam(1);
+
+        boolean didUserWin = game.didUserWin(1);
+
+        assertEquals(true, didUserWin);
+
+    }
+
+    @Test
+    public void checkForWinDiagonalTest() {
+        Game game = new Game(new ArrayList<Space>());
+
+        game.instantiateSpaces();
+
+        game.getGameBoxes().get(0).setTeam(1);
+        game.getGameBoxes().get(4).setTeam(1);
+        game.getGameBoxes().get(8).setTeam(1);
+
+        boolean didUserWin = game.didUserWin(1);
+
+        assertEquals(true, didUserWin);
+
+    }
+
+    @Test
+    public void userHasntWonYetTest() {
+        Game game = new Game(new ArrayList<Space>());
+
+        game.instantiateSpaces();
+
+        game.getGameBoxes().get(0).setTeam(1);
+        game.getGameBoxes().get(5).setTeam(1);
+        game.getGameBoxes().get(8).setTeam(1);
+
+        boolean didUserWin = game.didUserWin(1);
+
+        assertEquals(false, didUserWin);
+
+    }
+
 }
